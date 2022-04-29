690e8fa1269ac77a1ce747b1ae9870523a81ed64
CamrenLandry <42243738+CamrenLandry@users.noreply.github.com>
2022-04-29 00:43:26 -0400

    added login page and a js file for login

---
+++ loginjs.js
@@ -0,0 +1,10 @@
+function checkSeller(){
+    var y = document.getElementById("admin");
+    y.checked = false;
+  }
+
+function checkAdmin(){
+    var y = document.getElementById("user_or_seller");
+    y.checked = false;
+    console.log("admin");
+}
