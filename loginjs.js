82827c7e9307ceedf00b73916432ce1b8f33818c
CamrenLandry <42243738+CamrenLandry@users.noreply.github.com>
2022-05-02 16:42:31 -0400

    filters for dashboard

--- loginjs.js
+++ loginjs.js
@@ -25,6 +25,7 @@ function showForSale(){
   document.getElementById("favorite_cards").style.display = "none";
   document.getElementById("listings").style.display = "none";
   document.getElementById("filters").style.display = "none";
+  displayFilters();
 }
 
 function showListings(){
@@ -62,5 +63,53 @@ function makeCard() {
     clone.id = "dup" + i++;
     elem.after(clone);
     console.log("hey there");
+}
+
+function displayFilters(){
+    var minRent = document.getElementById("rentPrice").value;
+    var numBed = document.getElementById("bedrooms").value;
+    var numBath = document.getElementById("bathrooms").value;
+    var sqrft = document.getElementById("sqrfootage").value;
+    var subLength = document.getElementById("length").value;
+    var pets = document.getElementById("pets").value;
+    var city = document.getElementById("city").value;
+    var parking = document.getElementById("parking").value;
+    var age = document.getElementById("age").value;
+    
+    for(var j = 1; j < 7; j++){
+      var boolval = true;
+      var house = document.getElementById("house" + j);
+      var room = document.getElementById("rooms" + j).textContent;
+      var bath = document.getElementById("bath" + j).textContent;
+      var rent = document.getElementById("rent" + j).textContent;
+      var size = document.getElementById("size" + j).textContent;
+      var length = document.getElementById("length" + j).textContent;
+      var pet = document.getElementById("pets" + j).textContent;
+      var park = document.getElementById("parking" + j).textContent;
+      var date = document.getElementById("age" + j).textContent;
+      
+      
+      if(!(room >= numBed)){
+        boolval = false;
+      }else if(!(bath >= numBath)){
+        boolval = false;
+      }else if((rent <= minRent)){
+        boolval = false;
+      }else if(!(pet === pets) && !(pets === "placeholderPets")){
+        boolval = false;
+      }else if(!(park === parking) && !(parking === "placeholderPark")){
+        boolval = false;
+      }else if(!(date === age) && !(age === "placeholderAge")){
+        boolval = false;
+      }
+
+      if(!boolval){
+        house.style.display = "none";
+      }else{
+        house.style.display = "block";
+      }
+      
+      
+    }
 }
 
